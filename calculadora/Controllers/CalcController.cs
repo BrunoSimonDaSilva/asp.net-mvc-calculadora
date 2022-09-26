d4dec71edf59b0b855a0369128ddfba43cea40c0
Bruno Simon Da Silva <bruno.silva@mobilus.com.br>
2022-09-26 15:48:58 -0300

    codigo inicial

---
+++ calculadora/Controllers/CalcController.cs
@@ -0,0 +1,40 @@
+﻿using calculadora.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calculadora.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calcular(CalcModel calcular)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (calcular.operation)
+                {
+                    case "Soma":
+                        calcular.result = calcular.fist + calcular.second;
+                        return View("Index", calcular);
+                    case "Subtrair":
+                        calcular.result = calcular.fist - calcular.second;
+                        return View("Index", calcular);
+                    case "multiplicar":
+                        calcular.result = calcular.fist * calcular.second;
+                        return View("Index", calcular);
+                    case "dividir":
+                        calcular.result = calcular.fist / calcular.second;
+                        return View("Index", calcular);
+                    default:
+                        calcular.result = 0000;
+                        return View("Index", calcular);
+                }
+            }
+            return View("Index",calcular);
+        }
+
+    }
+}
---
+++ calculadora/Models/CalcModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace calculadora.Models
+{
+    public class CalcModel
+    {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public double fist { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public double second { get; set; }
+        public string operation { get; set; }
+        public double result { get; set; }
+    }
+}
